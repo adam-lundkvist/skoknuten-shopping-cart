1d7664a4a543f6c4873ee0ed765c249cfafed4c4
adam-lundkvist <113598210+adam-lundkvist@users.noreply.github.com>
2022-11-23 16:37:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ function clearCart() {
 
 function checkOut() {
     const cartContainer = document.querySelector(".cart-content");
-    if (cartContainer != null) {
+    if (cartContainer !== null) {
     initCart();
     renderCart();
     document.getElementById("summary").innerHTML = `Tack för ditt köp! Din order har tagits emot`;
